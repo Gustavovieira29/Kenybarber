d0c884e76237dbbd76547813238ced8adae948a7
abelfelipezwierzykowski <abelfelipesamas@gmail.com>
2025-09-03 20:10:53 -0300

    eererer

--- src/consultas.js
+++
@@ -1,9 +0,0 @@
-const db = require('./cnx')
-
-async function listarCursos() {
-    await db.connect()
-    let resultado = await db.query('select * from vw_agendamentos_detalhados')
-    console.log(resultado.rows)
-}
-
-listarCursos()
